464a389bf379605e423d955819db9f57589ab715
GuilhermeBDias <guilhermebdias55@gmail.com>
2024-12-26 13:38:14 -0300

    Build ➕
    Adicionei o cors

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { validateFieldStatus, validateFieldTitle } from '../middlewares/tasksMiddleware.js';
 import { fetchAllTasks, addTask, removeTask, editTask } from '../controllers/taskcontroller.js';
 
 // Routes for tasks
 const routes = (app) =>{
     app.use(express.json());
+    app.use(cors());
     app.get('/tasks', fetchAllTasks);
     app.post('/tasks',validateFieldTitle, addTask);
     app.delete('/tasks/:id', removeTask);
